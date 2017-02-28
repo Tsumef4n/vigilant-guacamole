050fa36645df787f91ad8360d7c9941451012565
Tsumef4n <tsumef4n@yahoo.de>
2017-02-28 16:28:51 +0100

    beautify

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -9,57 +9,70 @@ use Respect\Validation\Validator as v;
 
 class AdminController extends Controller
 {
-  public function getList($request, $response, $args)
-  {
-    $id = 0;
-    if (isset($args['id']))
+    public function getList($request, $response, $args)
     {
-      $id = $args['id'];
-      $products = Product::with('maker')->where('maker_id', '=', $id)->orderBy('name')->get();
-    }
-    else
-    {
-      $products = Product::with('maker')->orderBy('name')->get();
-    }
-    $makers = Maker::where('status', '!=', 2)->orderBy('name')->get();
+        $id = 0;
+        if (isset($args['id'])) {
+            $id = $args['id'];
+            $products = Product::with('maker')->where('maker_id', '=', $id)->orderBy('name')->get();
+        } else {
+            $products = Product::with('maker')->orderBy('name')->get();
+        }
+        $makers = Maker::where('status', '!=', 2)->orderBy('name')->get();
 
-    return $this->container->view->render($response, 'admin/admin.list.twig', [
-          'title' => "Warenangebot",
-          'active' => "admin",
+        return $this->container->view->render($response, 'admin/admin.list.twig', [
+          'title' => 'Warenangebot',
+          'active' => 'admin',
           'products' => $products,
           'makers' => $makers,
           'id' => $id,
       ]);
-  }
-
-  public function postNewProduct($request, $response)
-  {
-    $validation = $this->container->validator->validate($request, [
-      'name' => v::notEmpty(),
-      'description' => v::notEmpty(),
-    ]);
-
-    if ($validation->failed())
-    {
-      $this->container->flash->addMessage('error', 'Fehler bei der Erstellung!');
-      return $response->withRedirect($this->container->router->pathFor('admin.list'));
     }
+
+    public function postNewProduct($request, $response)
+    {
+        $validation = $this->container->validator->validate($request, [
+          'name' => v::notEmpty(),
+          'description' => v::notEmpty(),
+        ]);
+
+        if ($validation->failed()) {
+            $this->container->flash->addMessage('error', 'Fehler bei der Erstellung!');
+
+            return $response->withRedirect($this->container->router->pathFor('admin.list'));
+        }
 
     // getParam uses name of element, not the id
     $user = Product::create([
       'name' => $request->getParam('name'),
       'description' => $request->getParam('description'),
-      'maker_id' => 1
+      'maker_id' => 1,
     ]);
 
-    $this->container->flash->addMessage('info', 'Produkt erfolgreich hinzugefügt!');
+        $this->container->flash->addMessage('info', 'Produkt erfolgreich hinzugefügt!');
 
-    return $response->withRedirect($this->container->router->pathFor('admin.list'));
-  }
+        return $response->withRedirect($this->container->router->pathFor('admin.list'));
+    }
 
-  public function putUpdateProduct($request, $response, $args)
-  {
-    echo "PUT";
-    die();
-  }
+    public function putUpdateProduct($request, $response, $args)
+    {
+        $validation = $this->container->validator->validate($request, [
+          'name' => v::notEmpty(),
+          'description' => v::notEmpty(),
+        ]);
+
+        if ($validation->failed()) {
+            $this->container->flash->addMessage('error', 'Fehler beim Update!');
+
+            return $response->withRedirect($this->container->router->pathFor('admin.list'));
+        }
+
+        $product = Product::where('id', $args['id'])->update([
+            'name' => $request->getParam('name'),
+            'description' => $request->getParam('description'),
+        ]);
+        $this->container->flash->addMessage('info', 'Produkt erfolgreich geupdatet!');
+
+        return $response->withRedirect($this->container->router->pathFor('admin.list'));
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-  protected $table = 'products'; // not needed, eloquent sucht nach Pluralform vom Klassennamen
+    protected $table = 'products'; // not needed, eloquent sucht nach Pluralform vom Klassennamen
 
   protected $fillable = [
     'name',
     'description',
   ];
 
-  public function maker()
-  {
-      return $this->belongsTo('App\Models\Maker', 'maker_id', 'id');
-  }
-};
+    public function maker()
+    {
+        return $this->belongsTo('App\Models\Maker', 'maker_id', 'id');
+    }
+}
